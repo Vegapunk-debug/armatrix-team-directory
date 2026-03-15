df71c2971f2822b07f4cde2af06f6f4c07e71207
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-03-15 13:50:40 +0530

    Add ambient glow background effect to HomePage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,6 +7,9 @@ export default function HomePage() {
       <Navbar />
       <main className="relative min-h-screen overflow-hidden bg-ax-black">
 
+        <div className="pointer-events-none absolute inset-0">
+          <div className="absolute left-1/3 top-1/3 h-[400px] w-[400px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white/[0.03] blur-[100px]" />
+        </div>
       </main>
       <Footer />
     </>
